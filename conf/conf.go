cf0867518a1f16b9b3a61eeb5cfedd7ff2a5107b
shi.wang <im_alan@163.com>
2022-08-14 17:06:25 +0800

    debug

--- conf/conf.go
+++ conf/conf.go
@@ -1,21 +1,33 @@
 package conf
 
-import "gopkg.in/ini.v1"
+import (
+	"fmt"
 
-// const (
-// 	AppMode string
-// 	HttpPort string
-// 	RedisAddr string
-// 	RedisPwd string
-// 	RedisDbName string
-// 	Db string
-// 	DbHost string
-// 	DbPort string
-// 	DbUser string
-// 	DbName string
-// 	DbPasswd string
-// )
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode     string
+	HttpPort    string
+	RedisAddr   string
+	RedisPwd    string
+	RedisDbName string
+	Db          string
+	DbHost      string
+	DbPort      string
+	DbUser      string
+	DbName      string
+	DbPasswd    string
+)
 
 func Init() {
-	file, err := ini.Load("./config.ini")
+	file, err := ini.Load("./conf/config.ini")
+	if err != nil {
+		fmt.Println("配置文件读取错误，请检查文件路径！")
+	}
+
+	LoadServer(file)
+}
+
+func LoadServer(file *.ini.File) {
 }
