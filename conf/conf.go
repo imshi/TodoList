1d30ca8c14305db105b7dc28eca28b84c4b3385d
shi.wang <im_alan@163.com>
2022-08-14 17:21:34 +0800

    add .gitkeep

--- conf/conf.go
+++ conf/conf.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"strings"
 
 	"gopkg.in/ini.v1"
 )
@@ -17,7 +18,7 @@ var (
 	DbPort      string
 	DbUser      string
 	DbName      string
-	DbPasswd    string
+	DbPassWord  string
 )
 
 func Init() {
@@ -27,7 +28,21 @@ func Init() {
 	}
 
 	LoadServer(file)
+	LoadMysqlData(file)
+	path := strings.Join([]string{DbUser, ":", DbPassWord, "@tcp(", DbHost, ":", DbPort, ")/", DbName, "?charset=utf8&parseTime=true"}, "")
+	// model.Database(path)
 }
 
-func LoadServer(file *.ini.File) {
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("service").Key("AppMode").String()
+	HttpPort = file.Section("service").Key("HttpPort").String()
+}
+
+func LoadMysqlData(file *ini.File) {
+	Db = file.Section("mysql").Key("Db").String()
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassWord = file.Section("mysql").Key("DbPassWord").String()
+	DbName = file.Section("mysql").Key("DbName").String()
 }
